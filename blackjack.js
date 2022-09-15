ea147f9867dd1530262aeaa9bf603279ea8b0f84
Tolemy Kashyap <kashyaptolemy@gmail.com>
2022-09-15 20:20:15 +0530

    added opponent

--- blackjack.js
+++ blackjack.js
@@ -1,73 +1,127 @@
-let firstCard
-let secondCard
-let nextCard
-let sum = 0
+let firstCardp1
+let secondCardp1
+let nextCardp1
+let firstCardp2
+let secondCardp2
+let nextCardp2
+let sump1 = 0
+let sump2 = 0
 let message = ""
 
 let message_el = document.getElementById("message-El")
-let card_el = document.getElementById("card-el")
-let sum_el = document.getElementById("sum-el")
+let card_elp1 = document.getElementById("card-elp1")
+let sum_elp1 = document.getElementById("sum-elp1")
+let card_elp2 = document.getElementById("card-elp2")
+let sum_elp2 = document.getElementById("sum-elp2")
 
-let cardarr = []
+let cardarrp1 = []
+let cardarrp2 = []
 
 let newCardBtn = document.querySelector('.newCard')
 newCardBtn.style.visibility = 'hidden'
+let passBtn = document.querySelector('.pass')
+passBtn.style.visibility = 'hidden'
 
+function lost(){
+  message = "You lost :( . Wanna try again?"
+  newCardBtn.style.visibility = 'hidden'
+  passBtn.style.visibility = 'hidden'
+}
+function won(){
+  message = " Wohoo!! You Won !"
+  newCardBtn.style.visibility = 'hidden'
+  passBtn.style.visibility = 'hidden'
+}
+function draw_pass(){
+  message ="Would u like to draw a card or pass?"
+  newCardBtn.style.visibility = 'visible'
+  passBtn.style.visibility = 'visible'
+}
 function startgame(){
-    firstCard = Math.floor(Math.random()*9) + 2;
-    secondCard = Math.floor(Math.random()*9) + 2;
+    firstCardp1 = Math.floor(Math.random()*12) + 2;
+    secondCardp1 = Math.floor(Math.random()*12) + 2;
+    firstCardp2 = Math.floor(Math.random()*12) + 2;
+    secondCardp2 = Math.floor(Math.random()*12) + 2;
     
-    sum = firstCard+secondCard;
+    sump1 = firstCardp1+secondCardp1;
+    sump2 = firstCardp2+secondCardp2;
     
-    cardarr = []
-    cardarr.push(firstCard,secondCard)
+    cardarrp1 = []
+    cardarrp1.push(firstCardp1,secondCardp1)
+    cardarrp2 = []
+    cardarrp2.push(firstCardp2,secondCardp2)
     
-    if(sum<21){
-        message ="Would u like to draw a card?"
-        newCardBtn.style.visibility = 'visible'
+    if(sump1<21){
+        draw_pass()
 
     }    
-    else if(sum==21){
-        message ="Congrats You won!!"
-        newCardBtn.style.visibility = 'hidden'
+    else if(sump1==21){
+        won()
     }
-    else{
-    message = " You lost"
-    newCardBtn.style.visibility = 'hidden'
+    else if(sump2==21){
+        lost()
     }
-      console.log(sum)
+    else if(sump2>21){
+        won()
+    }
+    else if(sump1>21){
+        lost()
+    }
+      console.log(sump1)
       message_el.innerText = message;
-      card_el.innerText = "Cards: "+ firstCard+" "+secondCard;
-      sum_el.innerText = "Sum: "+ sum;
+      card_elp1.innerText = "Cards: "+ firstCardp1+" "+secondCardp1;
+      sum_elp1.innerText = "Sum: "+ sump1;
+      card_elp2.innerText = "Opponent Cards: "+ firstCardp2+" "+secondCardp2;
+      sum_elp2.innerText = "Sum: "+ sump2;
     }
 
+
 function newcard(){
-      nextCard = Math.floor(Math.random()*10)+2;
-      sum = sum+ nextCard;
-      cardarr.push(nextCard);
-      if(sum<21){
-        message ="Would u like to draw a card?"
-        newCardBtn.style.visibility = 'visible'
+      nextCardp1 = Math.floor(Math.random()*10)+2;
+      sump1 = sump1+ nextCardp1;
+      cardarrp1.push(nextCardp1);
+      if(sump1<21){
+        draw_pass()
       }    
-      else if(sum==21){
-        message ="Congrats You won!!"
-        newCardBtn.style.visibility = 'hidden'
+      else if(sump1==21){
+        won()
       }
       else{
-        message =" You lost"
-        newCardBtn.style.visibility = 'hidden'
+        lost()
         
       }
       console.log(message)
       message_el.innerText = message;
-      card_el.innerText = "Cards: ";
-      for(let i =0;i<cardarr.length;i++)
+      card_elp1.innerText = "Cards: ";
+      for(let i =0;i<cardarrp1.length;i++)
       {
-        card_el.textContent += cardarr[i] +" "
+        card_elp1.textContent += cardarrp1[i] +" "
 
       }
 
-      sum_el.innerText = "Sum: "+ sum;
+      sum_elp1.innerText = "Sum: "+ sump1;
 
+}
+function pass(){
+
+  while(1) {
+    if(sump2 > 21) {
+      won()
+      break
+    }
+    else if(sump2 == 21 || sump1 < sump2) {
+      won()
+      break
+    }
+    nextCardp2 = Math.floor(Math.random() * 10 ) + 1
+    cardarrp2.push(nextCardp2)
+    card_elp2.innerText = "Opponent Cards: ";
+    for(let i =0;i<cardarrp2.length;i++){
+      card_elp2.textContent += cardarrp2[i] + " "
+    }
+    sump2 += nextCardp2 
+    sum_elp2.innerText = "Sum: "+ sump2;
+
+  }
 }
     
