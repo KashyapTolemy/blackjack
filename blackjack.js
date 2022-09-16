e6ba737d65c765cc31d5bb9b9a80400917de981c
Tolemy Kashyap <kashyaptolemy@gmail.com>
2022-09-16 20:29:24 +0530

    bugfixes30

--- blackjack.js
+++ blackjack.js
@@ -13,6 +13,7 @@ let card_elp1 = document.getElementById("card-elp1")
 let sum_elp1 = document.getElementById("sum-elp1")
 let card_elp2 = document.getElementById("card-elp2")
 let sum_elp2 = document.getElementById("sum-elp2")
+message_el.style.transitionDuration = "2s"
 card_elp1.textContent=""
 sum_elp1.textContent=""
 card_elp2.textContent=""
@@ -41,11 +42,16 @@ function draw_pass(){
   newCardBtn.style.visibility = 'visible'
   passBtn.style.visibility = 'visible'
 }
+function draw(){
+  message ="Game drawn. Wanna play again?"
+  newCardBtn.style.visibility = 'hidden'
+  passBtn.style.visibility = 'hidden'
+}
 function startgame(){
-  firstCardp1 = Math.floor(Math.random()*12) + 2;
-  secondCardp1 = Math.floor(Math.random()*12) + 2;
-  firstCardp2 = Math.floor(Math.random()*12) + 2;
-  secondCardp2 = Math.floor(Math.random()*12) + 2;
+  firstCardp1 = Math.floor(Math.random()*8) + 2;
+  secondCardp1 = Math.floor(Math.random()*8) + 2;
+  firstCardp2 = Math.floor(Math.random()*8) + 2;
+  secondCardp2 = Math.floor(Math.random()*8) + 2;
   
   sump1 = firstCardp1+secondCardp1;
   sump2 = firstCardp2+secondCardp2;
@@ -56,7 +62,10 @@ function startgame(){
   cardarrp2.push(firstCardp2,secondCardp2)
   
   if(sump1<21 && sump2<21 ) draw_pass()
-  else if(sump1==21) won()
+  else if(sump1==21){
+    pass() 
+    return
+  }
   else if(sump2==21) lost()
   else if(sump2>21) won()
   else if(sump1>21) lost()
@@ -70,14 +79,14 @@ function startgame(){
   sum_elp2.innerText = "Sum: "+ sump2;
 }
 function newcard(){
-  nextCardp1 = Math.floor(Math.random()*11)+1;
-  nextCardp2 = Math.floor(Math.random()*11)+1;
+  nextCardp1 = Math.floor(Math.random()*8)+1;
+  // nextCardp2 = Math.floor(Math.random()*11)+1;
   sump1 += nextCardp1;
-  sump2 += nextCardp2;
+  // sump2 += nextCardp2;
   cardarrp1.push(nextCardp1);
-  cardarrp2.push(nextCardp2);
+  // cardarrp2.push(nextCardp2);
   
-  if(sump1 == 21 || (sump2 > 21 && sump1<21) ) won()
+  if((sump2 > 21 && sump1<21)) won()
   else if(sump1 > 21 || sump2 == 21) lost()
 
   console.log(message)
@@ -94,22 +103,32 @@ function newcard(){
   }
   
   sum_elp1.innerText = "Sum: "+ sump1;
-  sum_elp2.innerText = "Sum: "+ sump2;
+  if(sump1==21){
+    pass()
+    return
+  }
+  // sum_elp2.innerText = "Sum: "+ sump2;
 }
 function pass(){
+
   while(1) {
     if(sump2 > 21) {
       won()
       message_el.innerText = message
       break
     }
-    else if(sump2 == 21 || sump1 < sump2) {
+    else if( sump1 < sump2) {
       lost()
       message_el.innerText = message
       break
     }
+    else if(sump1==21 && sump2==21){
+      draw()
+      message_el.innerText =message
+      break
+    }
 
-    nextCardp2 = Math.floor(Math.random() * 10 ) + 1
+    nextCardp2 = Math.floor(Math.random() * 8 ) + 1
     cardarrp2.push(nextCardp2)
     card_elp2.innerText = "Opponent Cards: ";
     for(let i =0;i<cardarrp2.length;i++){
